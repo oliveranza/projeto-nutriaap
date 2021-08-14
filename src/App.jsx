80fe147ea9200375687fdc532e172b7ecc6d312f
Evandson Oliveira <oliveranza@gmail.com>
2021-08-14 19:21:22 -0300

    Revert "Barra de Menu para as telas de Administrador"
    This reverts commit 4680d6e68d9c3ae1a635ec0643cb07d4b3661834.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css';
 import InicioNutri from './components/Pages/InicioNutri/InicioNutri';
 import Login1 from './components/Pages/login/Login';
 import Recuperacao from './components/Pages/Recupercaosenha/Recuperacao';
-import CadastroNutri from './components/Pages/CadastroNutri/CadastroNutri'
+
 
 
 
@@ -13,8 +13,7 @@ function App() {
         <div className="App">
             {/* <InicioNutri></InicioNutri> */}
             {/* <Login1></Login1> */}
-            {/*<Recuperacao/>*/}
-            <CadastroNutri></CadastroNutri>
+            <Recuperacao/>
 
         </div>
     );
